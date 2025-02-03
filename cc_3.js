f72be16d58aaf7e406053d4a73557dc849c3617a
bojokoeva <bojokoeva34@gmail.com>
2025-02-02 22:31:17 -0500

    Task 1: Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,6 @@
+// Task 1: Product Price Management
+
+let prices = [15, 25, 35, 45, 55];
+prices.push(65); // Add new price
+prices = prices.slice(1); // Remove the first price 
+console.log("Updated Prices:", prices);
